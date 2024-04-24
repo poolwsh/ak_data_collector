4f05ec501dc062f964a1ece3a84c276da18bc0b5
wsh <4h8g5m800g@txy.com>
2024-04-24 13:03:14 +0800

    implement board dags

--- dags/dg_ak/sqls/init.sql
+++ dags/dg_ak/sqls/init.sql
@@ -64,3 +64,255 @@ CREATE TABLE ak_dg_stock_zt_pool_em_store (
     industry VARCHAR(100),          -- 所属行业
     PRIMARY KEY (td, s_code)        -- 将交易日期和股票代码组合作为主键
 );
+
+
+
+
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_name_ths;
+CREATE TABLE ak_dg_stock_board_concept_name_ths (
+    date DATE NOT NULL,
+    b_create_date DATE NOT NULL,
+    b_code VARCHAR(50),
+    b_name VARCHAR(255) NOT NULL,
+    stock_count INT,
+    url VARCHAR(255),
+    PRIMARY KEY (date,  b_code)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_name_ths_store;
+CREATE TABLE ak_dg_stock_board_concept_name_ths_store (
+    date DATE NOT NULL,
+    b_create_date DATE NOT NULL,
+    b_code VARCHAR(50),
+    b_name VARCHAR(255) NOT NULL,
+    stock_count INT,
+    url VARCHAR(255),
+    PRIMARY KEY (date,  b_code)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_name_em;
+CREATE TABLE ak_dg_stock_board_concept_name_em (
+    date DATE NOT NULL,
+    rank INT,
+    b_code VARCHAR(50),
+    b_name VARCHAR(255),
+    c DECIMAL,
+    change_amount DECIMAL,
+    change_percentage DECIMAL,
+    total_mv BIGINT,
+    turnover_rate DECIMAL,
+    risers INT,
+    fallers INT,
+    leader_s VARCHAR(255),
+    leader_s_change DECIMAL,
+    PRIMARY KEY (date, b_code)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_name_em_store;
+CREATE TABLE ak_dg_stock_board_concept_name_em_store (
+    date DATE NOT NULL,
+    rank INT,
+    b_code VARCHAR(50),
+    b_name VARCHAR(255),
+    c DECIMAL,
+    change_amount DECIMAL,
+    change_percentage DECIMAL,
+    total_mv BIGINT,
+    turnover_rate DECIMAL,
+    risers INT,
+    fallers INT,
+    leader_s VARCHAR(255),
+    leader_s_change DECIMAL,
+    PRIMARY KEY (date, b_code)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_summary_ths;
+CREATE TABLE ak_dg_stock_board_industry_summary_ths (
+    date DATE NOT NULL,
+    b_name VARCHAR(255),
+    change_percentage DECIMAL,
+    v DECIMAL,
+    a DECIMAL,
+    net_inflow DECIMAL,
+    risers INT,
+    fallers INT,
+    average_price DECIMAL,
+    leader_s VARCHAR(255),
+    leader_s_price DECIMAL,
+    leader_s_change DECIMAL,
+    PRIMARY KEY (date, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_summary_ths_store;
+CREATE TABLE ak_dg_stock_board_industry_summary_ths_store (
+    date DATE NOT NULL,
+    b_name VARCHAR(255),
+    change_percentage DECIMAL,
+    v DECIMAL,
+    a DECIMAL,
+    net_inflow DECIMAL,
+    risers INT,
+    fallers INT,
+    average_price DECIMAL,
+    leader_s VARCHAR(255),
+    leader_s_price DECIMAL,
+    leader_s_change DECIMAL,
+    PRIMARY KEY (date, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_name_em;
+CREATE TABLE ak_dg_stock_board_industry_name_em (
+    date DATE NOT NULL,
+    rank INT,
+    b_code VARCHAR(50),
+    b_name VARCHAR(255),
+    c DECIMAL,
+    change_amount DECIMAL,
+    change_percentage DECIMAL,
+    total_mv BIGINT,
+    turnover_rate DECIMAL,
+    risers INT,
+    fallers INT,
+    leader_s VARCHAR(255),
+    leader_s_change DECIMAL,
+    PRIMARY KEY (date, b_code)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_name_em_store;
+CREATE TABLE ak_dg_stock_board_industry_name_em_store (
+    date DATE NOT NULL,
+    rank INT,
+    b_code VARCHAR(50),
+    b_name VARCHAR(255),
+    c DECIMAL,
+    change_amount DECIMAL,
+    change_percentage DECIMAL,
+    total_mv BIGINT,
+    turnover_rate DECIMAL,
+    risers INT,
+    fallers INT,
+    leader_s VARCHAR(255),
+    leader_s_change DECIMAL,
+    PRIMARY KEY (date, b_code)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_cons_ths;
+CREATE TABLE ak_dg_stock_board_concept_cons_ths (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_cons_ths_store;
+CREATE TABLE ak_dg_stock_board_concept_cons_ths_store (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_cons_em;
+CREATE TABLE ak_dg_stock_board_concept_cons_em (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_concept_cons_em_store;
+CREATE TABLE ak_dg_stock_board_concept_cons_em_store (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_cons_ths;
+CREATE TABLE ak_dg_stock_board_industry_cons_ths (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    c DECIMAL(10, 2),
+    pct_chg DECIMAL(10, 2),
+    change DECIMAL(10, 2),
+    increase_speed DECIMAL(10, 2),
+    turnover_rate DECIMAL(10, 2),
+    quantity_ratio DECIMAL(10, 2),
+    amplitude DECIMAL(10, 2),
+    a VARCHAR(50),
+    circulating_shares VARCHAR(50),
+    circulation_mv DECIMAL(20, 2),
+    pe DECIMAL(10, 2),
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_cons_ths_store;
+CREATE TABLE ak_dg_stock_board_industry_cons_ths_store (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    c DECIMAL(10, 2),
+    pct_chg DECIMAL(10, 2),
+    change DECIMAL(10, 2),
+    increase_speed DECIMAL(10, 2),
+    turnover_rate DECIMAL(10, 2),
+    quantity_ratio DECIMAL(10, 2),
+    amplitude DECIMAL(10, 2),
+    a VARCHAR(50),
+    circulating_shares VARCHAR(50),
+    circulation_mv DECIMAL(20, 2),
+    pe DECIMAL(10, 2),
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_cons_em;
+CREATE TABLE ak_dg_stock_board_industry_cons_em (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    c DECIMAL(10, 2),
+    pct_chg DECIMAL(10, 2),
+    change DECIMAL(10, 2),
+    v BIGINT,
+    a DECIMAL(20, 2),
+    amplitude DECIMAL(10, 2),
+    h DECIMAL(10, 2),
+    l DECIMAL(10, 2),
+    o DECIMAL(10, 2),
+    yesterday_c DECIMAL(10, 2),
+    turnover_rate DECIMAL(10, 2),
+    pe_dynamic DECIMAL(10, 2),
+    pb DECIMAL(10, 2),
+    PRIMARY KEY (date, s_code, b_name)
+);
+
+DROP TABLE IF EXISTS ak_dg_stock_board_industry_cons_em_store;
+CREATE TABLE ak_dg_stock_board_industry_cons_em_store (
+    date DATE NOT NULL,
+    s_code VARCHAR(50) NOT NULL,
+    s_name VARCHAR(255) NOT NULL,
+    b_name VARCHAR(255) NOT NULL,
+    c DECIMAL(10, 2),
+    pct_chg DECIMAL(10, 2),
+    change DECIMAL(10, 2),
+    v BIGINT,
+    a DECIMAL(20, 2),
+    amplitude DECIMAL(10, 2),
+    h DECIMAL(10, 2),
+    l DECIMAL(10, 2),
+    o DECIMAL(10, 2),
+    yesterday_c DECIMAL(10, 2),
+    turnover_rate DECIMAL(10, 2),
+    pe_dynamic DECIMAL(10, 2),
+    pb DECIMAL(10, 2),
+    PRIMARY KEY (date, s_code, b_name)
+);
---
+++ dags/dg_ak/utils/config.py
@@ -0,0 +1,8 @@
+
+POSTGRES_CONN_ID = "local_pgsql"
+TXY800_PGSQL_CONN_ID = "txy800_pgsql_ak"
+REDIS_CONN_ID = "local_redis_3"
+
+DEFAULT_EMAIL = "wshmxgz@gmail.com"
+DEFAULT_OWNER = "wsh"
+DEFAULT_RETRY_DELAY = 5  # in minutes
