8535496e9d03c6e81e47dd951735be3e4186bb94
wsl.laptop <poolwsh@wsl.laptop>
2024-06-05 08:30:46 +0200

    move file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -21,8 +21,8 @@ airflow webserver &
 
 # Run the custom initialization script after a delay to ensure Airflow is up
 sleep 30
-python /opt/airflow/tools/init_ak_dag_db.py
-python /opt/airflow/dags/dg_ak/store_daily/s-zh-a/init_ak_dg_s_zh_a.py
+# python /opt/airflow/tools/init_ak_dag_db.py
+# python /opt/airflow/dags/dg_ak/store_daily/s-zh-a/init_ak_dg_s_zh_a.py
 
 # Keep the container running
 tail -f /dev/null
